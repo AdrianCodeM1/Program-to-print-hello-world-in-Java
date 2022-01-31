64a238181f569d117d8092d5731e4e81df01299a
Adrian <98700970+AdrianCodeM1@users.noreply.github.com>
2022-01-31 06:30:53 -0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class FirstProgram  // FirstProgram is a class
+{
+    public static void main(String[] args)        // main is a method class
+    {
+        System.out.println("Hello World");
+    }
+}
