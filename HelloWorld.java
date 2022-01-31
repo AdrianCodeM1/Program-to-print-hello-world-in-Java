c1ba4dab1a81356098262a051d32a7164a00dd20
Adrian <98700970+AdrianCodeM1@users.noreply.github.com>
2022-01-31 06:31:54 -0800

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class FirstProgram  // FirstProgram is a class
-{
-    public static void main(String[] args)        // main is a method class
-    {
-        System.out.println("Hello World");
-    }
-}
